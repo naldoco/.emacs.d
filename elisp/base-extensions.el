7e3f8a85990ccea4be653fa6a3420bec5b174df0
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2017-02-09 15:09:19 +0100

    markdown-mode

--- elisp/base-extensions.el
+++ elisp/base-extensions.el
@@ -94,6 +94,14 @@
 
 (use-package magit-popup)
 
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 (use-package multiple-cursors
   :bind
   ("C-S-c C-S-c" . mc/edit-lines)
